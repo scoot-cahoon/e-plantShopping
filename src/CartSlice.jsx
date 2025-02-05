6aa18c8bbf86006b9a660291e8ca01815a518247
Paul <paul.pronio@hotmail.ca>
2025-02-05 18:35:09 -0500

    Task 3 Complete

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,12 +7,31 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+    const {name, image, cost} = action.payload;
+    //state.items access initalState and maps that value to new variable item and then compares agains the 
+    //name that was taken from line 10 payload object
+        const existingItem = state.items.find(item => item.name === name)
+        if (existingItem){
+            existingItem.quantity++;
+        }
+        else{
+            state.items.push({name, image, cost, quantity:1});
+        }
+
     },
     removeItem: (state, action) => {
+        const {name, image, cost} = action.payload;
+        const existingItem = state.items.find(item => item.name === name)
+        if (existingItem){
+            state.items = state.items.filter(item => item.name !== action.payload);
+        }
     },
     updateQuantity: (state, action) => {
-
+        const {name, quantity} = action.payload;
+        const existingItem = state.items.find(item => item.name === name)
+        if (existingItem){
+            existingItem.quantity = quantity;
+        }
     
     },
   },
--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,10 +1,13 @@
 import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import { useSelector, useDispatch } from "react-redux";
+import { addItem } from './CartSlice';
+
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
-
+    const [addedToCart, setAddedToCart] = useState({});
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -246,6 +249,9 @@ const handlePlantsClick = (e) => {
     e.preventDefault();
     setShowCart(false);
   };
+  const handleAddToCart = (e) => {
+
+  }
     return (
         <div>
              <div className="navbar" style={styleObj}>
@@ -268,7 +274,25 @@ const handlePlantsClick = (e) => {
         </div>
         {!showCart? (
         <div className="product-grid">
+            {plantsArray.map((cat, index) =>(
+                <div>
+                    <h1><div>{cat.category}</div></h1>
+                    <div className='product-list'>
+                        {cat.plants.map((plant, index) => (
+                            <div className='product-card' key={index}>
+                                <h2 className="product-title">{plant.name}</h2>
+                                <img className="product-image" src={plant.image}></img>
+                                <div className="product-description">{plant.description}</div>
+                                <div className="product-cost">{plant.cost}</div>
+                                <button className="product-button" onClick={() => handleAddToCart(plant)}>Add To Cart</button>
 
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            )
+        
+        )}
 
         </div>
  ) :  (
